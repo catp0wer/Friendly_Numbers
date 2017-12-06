49d34f7e6bcb0f237f3da8067ae56c6b212ec1e4
laila <laila@fake.com>
2017-12-06 22:59:26 +0200

    added functionlity which find teh divisors of a number

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+        FriendlyN my_numbers = new FriendlyN();
+        ArrayList<Integer> divisorsX = my_numbers.findDivisorX(50);
+
+
+    }
+}
