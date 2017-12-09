7a9eaf919df850314da4d1724baa76cde955b68c
laila <laila@fake.com>
2017-12-09 13:52:38 +0200

    adding functionality to enter the number from the keyboard and sending the pair number to be verified if they are friendly

--- src/com/company/FriendlyNumbers.java
+++ src/com/company/FriendlyNumbers.java
@@ -1,18 +1,30 @@
 package com.company;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class FriendlyNumbers {
+
+    public void sendNumbers(int n){
+
+        for(int i=n; i>0;i--){
+            for(int j=0;j<=n;j++){
+                if(isFriendly(i,j)){
+                    System.out.println("i " + i +" and j " + j + " are friendly ");
+                }
+            }
+        }
+    }
 
     private ArrayList<Integer> findDivisors(int number) {
         ArrayList<Integer> divisors = new ArrayList<Integer>();
 
         for (int j = number-1; j > 0; j--) {
             if (number % j == 0) {
-                System.out.println("x is " + number + " and divisors are " + j);
+              //  System.out.println("x is " + number + " and divisors are " + j);
                 divisors.add(j);
             }
         }
-        System.out.println(divisors);
+        //System.out.println(divisors);
         return divisors;
     }
 
@@ -22,12 +34,15 @@ public class FriendlyNumbers {
         //for (int i = 0; i < divisors.size(); i++) {
             sum += i;
         }
-        System.out.println(sum);
+       // System.out.println(sum);
         return sum;
     }
 
 
     public boolean isFriendly(int x, int y){
+        if(x == y){
+            return false;
+        }
         ArrayList<Integer> divisorsX = findDivisors(x);
         ArrayList<Integer> divisorsY = findDivisors(y);
         int sumX = sumDivisors(divisorsX);
@@ -36,5 +51,8 @@ public class FriendlyNumbers {
         return sumX == y && sumY==x;
 
     }
+
+
+
 
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,23 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
         FriendlyNumbers my_numbers = new FriendlyNumbers();
-        boolean result = my_numbers.isFriendly(220,284);
+
+        ///codul adaugat
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a digit ");
+        int n = scanner.nextInt();
+        my_numbers.sendNumbers(n);
+
+
+       boolean result = my_numbers.isFriendly(28,28);
+
+
+
        if(result){
            System.out.println("These numbers are friendly");
        }else{
